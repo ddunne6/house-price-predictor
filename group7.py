75a888fe539bc125c271d4a31832cf724bf07357
charliemaguire <62850416+charliemaguire@users.noreply.github.com>
2021-11-28 13:03:59 +0000

    Evaluation annd plotting

--- group7.py
+++ group7.py
@@ -14,7 +14,7 @@ from sklearn.preprocessing import PolynomialFeatures, OneHotEncoder, LabelEncode
 from sklearn.model_selection import KFold, train_test_split
 from sklearn.dummy import DummyClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import confusion_matrix, f1_score, roc_curve, mean_squared_error
+from sklearn.metrics import confusion_matrix, f1_score, roc_curve, mean_squared_error, r2_score
 
 df = pd.read_csv("ml-dataset.csv")
 print(df.head())
@@ -51,93 +51,152 @@ print(X)
 # Split training and test data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# C value cross validation
-mean_error=[]; std_error=[]
-# C values to validate
-CL_range = [0.0001,0.001,0.01,0.1]# Loop for each value
-for Ci in CL_range:
-    model = linear_model.LinearRegression()
-    temp=[]
-    kf = KFold(n_splits=5)
-    # Divide data into train and test
-    for train, test in kf.split(X):
-        model.fit(X[train], y[train])
-        ypred = model.predict(X[test])
-        temp.append(mean_squared_error(y[test],ypred))    # Calculate errors
-    mean_error.append(np.array(temp).mean())
-    std_error.append(np.array(temp).std())
-# Plot mean square error vs C value
-plt.errorbar(CL_range,mean_error,yerr=std_error)
-plt.xlabel('Ci'); plt.ylabel('MSE')
-plt.xlim((0,0.1))
-plt.title('Linear Regression - C Cross Validation')
-plt.show()
+# # C value cross validation
+# mean_error=[]; std_error=[]
+# # C values to validate
+# CL_range = [0.00001,0.0001,0.001,0.01,0.1, 1]# Loop for each value
+# for Ci in CL_range:
+#     model = linear_model.LinearRegression()
+#     temp=[]
+#     kf = KFold(n_splits=5)
+#     # Divide data into train and test
+#     for train, test in kf.split(X):
+#         model.fit(X[train], y[train])
+#         ypred = model.predict(X[test])
+#         temp.append(mean_squared_error(y[test],ypred))    # Calculate errors
+#     mean_error.append(np.array(temp).mean())
+#     std_error.append(np.array(temp).std())
+# # Plot mean square error vs C value
+# plt.errorbar(CL_range,mean_error,yerr=std_error)
+# plt.xlabel('Ci'); plt.ylabel('MSE')
+# plt.title('Linear Regression - C Cross Validation')
+# plt.xscale("log")
+# plt.show()
 
-# C value cross validation
-mean_error=[]; std_error=[]
-# C values to validate
-CL_range = [0.0001,0.001,0.01,0.1]# Loop for each value
-for Ci in CL_range:
-    model = linear_model.Lasso(alpha=1/(2*Ci), max_iter=10000)
-    temp=[]
-    kf = KFold(n_splits=5)
-    # Divide data into train and test
-    for train, test in kf.split(X):
-        model.fit(X[train], y[train])
-        ypred = model.predict(X[test])
-        temp.append(mean_squared_error(y[test],ypred))    # Calculate errors
-    mean_error.append(np.array(temp).mean())
-    std_error.append(np.array(temp).std())
-# Plot mean square error vs C value
-plt.errorbar(CL_range,mean_error,yerr=std_error)
-plt.xlabel('Ci'); plt.ylabel('MSE')
-plt.xlim((0,0.1))
-plt.title('Lasso Regression - C Cross Validation')
-plt.show()
+# # C value cross validation
+# mean_error=[]; std_error=[]
+# # C values to validate
+# CL_range = [0.00001,0.0001,0.001,0.01,0.1,1]# Loop for each value
+# for Ci in CL_range:
+#     model = linear_model.Lasso(alpha=1/(2*Ci), max_iter=10000)
+#     temp=[]
+#     kf = KFold(n_splits=5)
+#     # Divide data into train and test
+#     for train, test in kf.split(X):
+#         model.fit(X[train], y[train])
+#         ypred = model.predict(X[test])
+#         temp.append(mean_squared_error(y[test],ypred))    # Calculate errors
+#     mean_error.append(np.array(temp).mean())
+#     std_error.append(np.array(temp).std())
+# # Plot mean square error vs C value
+# plt.errorbar(CL_range,mean_error,yerr=std_error)
+# plt.xlabel('Ci'); plt.ylabel('MSE')
+# plt.title('Lasso Regression - C Cross Validation')
+# plt.xscale("log")
+# plt.show()
 
-# C value cross validation
-mean_error=[]; std_error=[]
-# C values to validate
-CL_range = [0.0001,0.001,0.01,0.1]# Loop for each value
-for Ci in CL_range:
-    model = linear_model.Ridge(alpha=1/(2*Ci), max_iter=1000)
-    temp=[]
-    kf = KFold(n_splits=5)
-    # Divide data into train and test
-    for train, test in kf.split(X):
-        model.fit(X[train], y[train])
-        ypred = model.predict(X[test])
-        temp.append(mean_squared_error(y[test],ypred))
-    # Calculate errors
-    mean_error.append(np.array(temp).mean())
-    std_error.append(np.array(temp).std())
-# Plot mean square error vs C value
-plt.errorbar(CL_range,mean_error,yerr=std_error)
-plt.xlabel('Ci'); plt.ylabel('MSE')
-plt.xlim((0,0.1))
-plt.title('Ridge Regression - C Cross Validation')
-plt.show()
+# # C value cross validation
+# mean_error=[]; std_error=[]
+# # C values to validate
+# CL_range = [0.00001,0.0001,0.001,0.01,0.1,1]# Loop for each value
+# for Ci in CL_range:
+#     model = linear_model.Ridge(alpha=1/(2*Ci), max_iter=1000)
+#     temp=[]
+#     kf = KFold(n_splits=5)
+#     # Divide data into train and test
+#     for train, test in kf.split(X):
+#         model.fit(X[train], y[train])
+#         ypred = model.predict(X[test])
+#         temp.append(mean_squared_error(y[test],ypred))
+#     # Calculate errors
+#     mean_error.append(np.array(temp).mean())
+#     std_error.append(np.array(temp).std())
+# # Plot mean square error vs C value
+# plt.errorbar(CL_range,mean_error,yerr=std_error)
+# plt.xlabel('Ci'); plt.ylabel('MSE')
+# plt.title('Ridge Regression - C Cross Validation')
+# plt.xscale("log")
+# plt.show()
 
-# Cross validation - KNN k
-mean_error=[]; std_error=[]
-# C values to validate
-K_range = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# Loop for each k
-for Ki in K_range:
-    model = KNeighborsClassifier(n_neighbors=Ki, weights='uniform')
-    temp=[]
-    kf = KFold(n_splits=5)
-    # Divide data into train and test
-    for train, test in kf.split(X):
-        model.fit(X[train], y[train])
-        ypred = model.predict(X[test])
-        temp.append(mean_squared_error(y[test],ypred))
-    # Calculate errors
-    mean_error.append(np.array(temp).mean())
-    std_error.append(np.array(temp).std())
-# Plot mean square error vs C value
-plt.errorbar(K_range,mean_error,yerr=std_error)
-plt.xlabel('Ki'); plt.ylabel('MSE')
-plt.xlim((0,10))
-plt.title('kNN - k Cross Validation')
+# # Cross validation - KNN k
+# mean_error=[]; std_error=[]
+# # C values to validate
+# K_range = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# # Loop for each k
+# for Ki in K_range:
+#     model = KNeighborsClassifier(n_neighbors=Ki, weights='uniform')
+#     temp=[]
+#     kf = KFold(n_splits=5)
+#     # Divide data into train and test
+#     for train, test in kf.split(X):
+#         model.fit(X[train], y[train])
+#         ypred = model.predict(X[test])
+#         temp.append(mean_squared_error(y[test],ypred))
+#     # Calculate errors
+#     mean_error.append(np.array(temp).mean())
+#     std_error.append(np.array(temp).std())
+# # Plot mean square error vs C value
+# plt.errorbar(K_range,mean_error,yerr=std_error)
+# plt.xlabel('Ki'); plt.ylabel('MSE')
+# plt.xlim((0,10))
+# plt.title('kNN - k Cross Validation')
+# plt.show()
+
+# Create Models
+# Linear Regression
+linear_mod = linear_model.LinearRegression()
+linear_mod.fit(X_train, y_train)
+linear_ypred = linear_mod.predict(X_test)
+linear_MSE = mean_squared_error(y_test,linear_ypred)
+linear_R2 = r2_score(y_test,linear_ypred)
+print("Linear - MSE = ", linear_MSE, ", R2 = ", linear_R2)
+
+# Lasso Regression
+lasso_model = linear_model.Lasso(alpha=1/(2*0.001), max_iter=10000)
+lasso_model.fit(X_train, y_train)
+lasso_ypred = lasso_model.predict(X_test)
+lasso_MSE = mean_squared_error(y_test,lasso_ypred)
+lasso_R2 = r2_score(y_test,lasso_ypred)
+print("Lasso - MSE = ", lasso_MSE, ", R2 = ", lasso_R2)
+
+# Ridge Regression
+ridge_model = linear_model.Ridge(alpha=1/(2*0.001), max_iter=1000)
+ridge_model.fit(X_train, y_train)
+ridge_ypred = ridge_model.predict(X_test)
+ridge_MSE = mean_squared_error(y_test,ridge_ypred)
+ridge_R2 = r2_score(y_test,ridge_ypred)
+print("Ridge - MSE = ", ridge_MSE, ", R2 = ", ridge_R2)
+
+# kNN Regression
+kNN_model = KNeighborsClassifier(n_neighbors=2, weights='uniform')
+kNN_model.fit(X_train, y_train)
+kNN_ypred = kNN_model.predict(X_test)
+kNN_MSE = mean_squared_error(y_test,kNN_ypred)
+kNN_R2 = r2_score(y_test,kNN_ypred)
+print("kNN - MSE = ", kNN_MSE, ", R2 = ", kNN_R2)
+
+# Baseline - Predicts the average value
+base_ypred = [y_train.mean()] * len(y_test)
+base_MSE = mean_squared_error(y_test,base_ypred)
+base_R2 = r2_score(y_test,base_ypred)
+print("Base - MSE = ", base_MSE, ", R2 = ", base_R2)
+
+X_train2 = zip(*X_train)
+X_train2 = list(X_train2)
+
+X_test2 = zip(*X_test)
+X_test2 = list(X_test2)
+
+# Plot predictions
+# 0 for income, 1 for besds, 2 for baths, 3 for area
+input = 1
+plt.rc('font', size=18)
+plt.rcParams['figure.constrained_layout.use'] = True
+plt.scatter(X_train2[input], y_train, color='green', marker='+')
+plt.scatter(X_test2[input], linear_ypred, color='red', marker='x')
+plt.scatter(X_test2[input], lasso_ypred, color='yellow', marker='o')
+plt.scatter(X_test2[input], ridge_ypred, color='black', marker='*')
+plt.scatter(X_test2[input], kNN_ypred, color='blue', marker='D')
+plt.xlabel("Average Income")
+plt.ylabel("House Price")
 plt.show()
